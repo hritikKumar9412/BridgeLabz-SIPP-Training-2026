5e94aa088d8e75b569741a66076426f9a2d545d1
hritikKumar9412 <hritikthakur200@gmail.com>
2026-06-17 15:21:26 +0530

    [Hritik Kumar].add committing the Exception Handling Questions

---
+++ core-java-practice/gcr-codebase/Exception Hnadling/ArrayExample.java
@@ -0,0 +1,28 @@
+package ExceptionHandling;
+
+import java.util.*;
+
+public class ArrayExample {
+    public static void main(String[] args) {
+
+        Integer[] arr = {10, 20, 30, 40, 50};
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter index: ");
+        int index = sc.nextInt();
+
+        try {
+            System.out.println("Value at index " + index +
+                    ": " + arr[index]);
+
+        } catch (ArrayIndexOutOfBoundsException e) {
+            System.out.println("Invalid index!");
+
+        } catch (NullPointerException e) {
+            System.out.println("Array is not initialized!");
+        }
+
+        sc.close();
+    }
+}
